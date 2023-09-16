93026bdd41e60f96efa13d2b54466736b57626de
山崎柊 <>
2023-09-16 10:59:16 +0900

    homeとaboutを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do #
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  #rootは”/”を意味する。特に指定がなければhomecontrollerのindexメソッドを実行。
+  #また、html.erbでroot_pathメソッドを実行すると"/"ページへのURLが生成される
+
+  #get '/' => 'home#index'
+  #↑ブラウザからサーバーへゲットリクエストが来た時にhomecontrollerのindexメソッドを実行。rootがあるのでコメントアウト
+  get '/about' => 'home#about' #同上でaboutメソッドを実行
 end
+
