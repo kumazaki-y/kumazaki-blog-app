c8d68ebdb7daf9f1237c75ec4576a63dac623bfe
山崎柊 <>
2023-10-11 08:27:54 +0900

    メール送付とコールバック

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  default from: 'from@example.com' #指定したアドレスからメールを送信するように設定。
+  layout 'mailer' #メールのレイアウトはviewsのmailerファイルを使用。
 end
